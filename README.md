ccaa13b5e5edc29a10dca2859442511149d26d51
Arina Bezdushnaya <arinabezdushnaya@gmail.com>
2021-08-02 11:33:19 +0300

    Deployment

---
+++ README.md
@@ -0,0 +1,10 @@
+# Userlist with filter
+:page_facing_up: :woman: :man:
+
+## Description
+The application fetch users data from the server, forms a table and allows filtering by username.
+
+## Usage
+After application loading you can see userlist with all required information about users.
+You can filter users by name. When nothing found you get message. Or you can use reset button to return initial list.
+![screen](https://user-images.githubusercontent.com/88073632/127828808-38c6f4d2-abd3-4736-b1f9-c32f5970b48e.png)
--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const userlist = document.querySelector("tbody");
+const userlistDisplayed = document.querySelector("tbody");
 const buttonReset = document.querySelector("button");
 const form = document.querySelector("form");
 const indicator = document.querySelector(".loading-indicator");
 const filter = document.querySelector(".filter");
 const userTable = document.querySelector(".wrapper");
+const filterNameInput = document.querySelector("input");
 
 
 
-//creates an instance of user-object with requried information
+//creates an instance of user-object with required information
 const person = class User {
   constructor(user) {
     this.username = `${user.name.first} ${user.name.last}`;
-    this.userPicture = user.picture.thumbnail;
+    this.userPictureThumbnail = user.picture.thumbnail;
+    this.userPictureTooltip = user.picture.large;
     this.userLocation = `${user.location.state} ${user.location.city}`;
     this.userEmail = user.email;
     this.userPhone = user.phone;
@@ -19,58 +21,122 @@ const person = class User {
   }
 }
 
-
 //get data from server, return object in json format
 const getUserlist = async () => {
   try {
     const response = await fetch('https://randomuser.me/api/?results=15');
     const json = await response.json();
-    return json;
+    return json.results;
   } catch (error) {
     console.log('Error:', error);
   }
 }
 
 
-//use json object to separate an array included all users
-//create an instance for all array elements and insert user info into html-tags
-async function displayUserlist(){
+// the decorator makes it possible to avoid unnecessary rerenders when typing in the filtering field
+function debounce(func, wait) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, wait);
+  };
+}
 
-  const users = await getUserlist();
 
-  users.results.forEach(user => {
-    let userInfo = new person(user);
-    const trCreate = userlist.appendChild(document.createElement("tr"));
-
-    const usernameInsert = trCreate.appendChild(document.createElement("td"));
-    usernameInsert.innerHTML = userInfo.username;
-
-    const userPictureInsert = trCreate.appendChild(document.createElement("td")).appendChild(document.createElement("img"));
-    userPictureInsert.src = userInfo.userPicture;
-
-    const userLocationInsert = trCreate.appendChild(document.createElement("td"));
-    userLocationInsert.innerHTML = userInfo.userLocation;
-
-    const userEmailInsert = trCreate.appendChild(document.createElement("td"));
-    userEmailInsert.innerHTML = userInfo.userEmail;
-
-    const userPhoneInsert = trCreate.appendChild(document.createElement("td"));
-    userPhoneInsert.innerHTML = userInfo.userPhone;
-
-    const userRegisteredDateInsert = trCreate.appendChild(document.createElement("td"));
-    userRegisteredDateInsert.innerHTML = userInfo.userRegisteredDate;
+//create an array of users only with nessesary info about them
+//and pass an array to filter and display function as parameter
+const  fillUserlistRequiredInfo = async ()=> {
+  const userData = await getUserlist();
+  const userlist = [];
+  userData.forEach(user => {
+    const userInfo = new person(user);
+    userlist.push(userInfo); 
   });
 
+  //pass an initial array and input value to filter
+  filterNameInput.addEventListener("keydown", debounce(function(event) {
+    event.preventDefault();
+    const filterName = filterNameInput.value;
+    filterUserlist(userlist, filterName);
+  }, 1000));
+
+  //reset input field and display an initial data in the table
+  buttonReset.addEventListener("click", function() { 
+    displayUserlist(userlist);
+    filterNameInput.value = "";
+  });
+
+  //pass an initial array to display
+  displayUserlist(userlist);
 };
 
 
-/*add loading indicator,
-data from server load fast enough so add artificial delay to show indicator
-and then show data in the table*/
+//insert info about all users into the table
+const displayUserlist = (userlist) => {
+  userlistDisplayed.innerHTML = "";
+  userlist.forEach(user => {
+    const tableRow = userlistDisplayed.appendChild(document.createElement("tr"));
+    const usernameInsert = tableRow.appendChild(document.createElement("td"));
+    const userPictureInsert = tableRow.appendChild(document.createElement("td"));
+    const userPictureThumbnail = userPictureInsert.appendChild(document.createElement("img"));
+    const userPictureTooltip = userPictureInsert.appendChild(document.createElement("img"));
+    const userLocationInsert = tableRow.appendChild(document.createElement("td"));
+    const userEmailInsert = tableRow.appendChild(document.createElement("td"));
+    const userPhoneInsert = tableRow.appendChild(document.createElement("td"));
+    const userRegisteredDateInsert = tableRow.appendChild(document.createElement("td"));
+    
+    usernameInsert.innerHTML = user.username;
+    userPictureThumbnail.src = user.userPictureThumbnail;
+    userPictureTooltip.src = user.userPictureTooltip;
+    userPictureTooltip.className = "tooltip";
+    userLocationInsert.innerHTML = user.userLocation;
+    userEmailInsert.innerHTML = user.userEmail;
+    userPhoneInsert.innerHTML = user.userPhone;
+    userRegisteredDateInsert.innerHTML = user.userRegisteredDate;
+  
+    //pass image elements to the tooltip as parameters
+    showTooltip(userPictureThumbnail, userPictureTooltip);
+  })
+};
+
+
+// data from server load fast enough so add artificial delay to show loading indicator
+// and then show data in the table
 window.addEventListener("load", function() {
-  displayUserlist();
-  this.setTimeout(() => {
+  fillUserlistRequiredInfo();
+    this.setTimeout(() => {
     indicator.style.display = "none";
     userTable.style.display = "block";
   }, 1000);
 });
+
+
+// error appears when filter found nothing
+const showError = () => {
+  const errorMessage = userlistDisplayed.appendChild(document.createElement("tr")).appendChild(document.createElement("td"));
+  errorMessage.colSpan = "6";
+  errorMessage.className = "error-message";
+  errorMessage.innerHTML = "Nothing Found";
+}
+
+// implement filter by username
+// filter takes an initial array and input value and then table is updated
+const filterUserlist = (userlist, filterName) => {
+  const filteredList = userlist.filter(user => user.username.toLowerCase().indexOf(filterName) != -1)
+  displayUserlist(filteredList);
+
+  if(filteredList.length === 0) {
+    showError();
+  }
+};
+
+
+// tooltip appears when you hover your cursor over a thumbnail and dissappear when mouse is out
+const showTooltip = (userPictureThumbnail, userPictureTooltip) => {
+  userPictureThumbnail.onmouseover =  function() {
+    userPictureTooltip.style.visibility = "visible";
+  };
+  userPictureThumbnail.onmouseout =  function() {
+    userPictureTooltip.style.visibility = "hidden";
+  };
+}
